ef1de18125c07f37f64cab2708f140251d6dc1d1
Huang Degil <huang.degil@gmail.com>
2018-11-27 08:08:07 +0900

    in progress

--- project/src/app/org-chart/tree/tree.component.ts
+++ project/src/app/org-chart/tree/tree.component.ts
@@ -11,16 +11,21 @@ export class TreeComponent {
   @Input() org: Org;
 
   drag(o, e) {
+    e.stopPropagation();
+    console.log("drag",o,e);
     e.dataTransfer.setData("text", e.target);
   }
 
   drop(o, e) {
     e.preventDefault();
     var data = e.dataTransfer.getData("text");
+    console.log("drop", data, o, e);
     e.target.appendChild(data);
   }
 
   allow(o, e) {
     e.preventDefault();
+    var data = e.dataTransfer.getData("text");
+    console.log("allow","("+data+")", o,e);
   }
 }
